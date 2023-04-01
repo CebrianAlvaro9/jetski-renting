c9aa408a9f70706c7e487e20d336c815b8d1eba5
CebrianAlvaro9 <113922067+CebrianAlvaro9@users.noreply.github.com>
2023-04-01 17:36:45 +0300

    aa

--- resources/views/client/rent/jetski-check.blade.php
+++ resources/views/client/rent/jetski-check.blade.php
@@ -4,27 +4,33 @@
        
         <form method="POST" action="{{ route('seleccion-moto') }}">
             @csrf
-        @foreach ($motoslibres as $moto)
+             @method("PUT")
+     
+        
             <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
                 {{-- <img class="w-full h-48" src="{{ Storage::url($moto->brand) }}" alt="Image" /> --}}
+                @foreach ($motoslibres as $moto)
+
                 <div class="px-6 py-4">
                     <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">
                         {{ $moto->brand }}</h4>
+                     <input type="radio" name="moto_seleccionada" value="{{ $moto->id }}" @if($loop->first) checked @endif>
                     <p class="leading-normal text-gray-700">{{ $moto->model }}.</p>
                 </div>
+             
                 <div class="flex items-center justify-between p-4">
                     <span class="text-xl text-green-600">{{ $moto->price }}$</span>
                 </div>
-                <input type=hidden name="id" value="{{ $moto->id }}" >
-                <input type="hidden" name="price_id" value="{{ $moto->price_id }}" >
-                <input type="hidden" name="price" value="{{ $moto->price }}" >
+              
+                @endforeach
                 <div class="mt-6 p-4 flex ">
                     <button type="submit"
                         class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Next</button>
                 </div>
             </div>
-        @endforeach
+        
     </form>
+
     </div>
 </div>
 </x-client-layout>
