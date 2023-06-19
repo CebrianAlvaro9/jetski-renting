90939ae38effde877c2bb4cba53fce348412e70a
Alvaro Cebrian urueña <cebrianalvaro9@gmail.com>
2023-06-19 21:15:41 +0200

    a

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -8,7 +8,7 @@
 <section class="bg-white py-3">
     <div  class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8  ">
         <div class="text-center">
-            <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Cont</span>acto</h1>
+            <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Contacto</span></h1>
             <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Aqui tienes nuestra información de contácto</p>
         </div>
 
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -5,7 +5,7 @@
 <section class="bg-white py-3">
   <div id="all" class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8  ">
     <div class="text-center">
-    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Nuestras gam</span>as de precio</h1>
+    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Nuestras gamas de precio</span></h1>
     <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">En Waveriders queremos ofrecer la oportunidad de vivir la expericiencia a todo tipo de personas por lo que nuestros precios se adaptan a todos los bolsillos</p>
     </div>
  
@@ -72,7 +72,7 @@
 
     <div id="entrada-info"class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8  ">
       <div class="text-center">
-      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Gama de</span> entrada</h1>
+      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Gama de entrada</span> </h1>
       <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Desde Waveriders queremos dar la mejor expericiencia posible al margen  del precio por ello en nuestra gama de entrada con un precio super ajustado de 80 euros te ofrecemos los siguiente. <a href="welcome"class="underline decoration-black-500"><b>Otros planes</b></a> </p> 
       <div class="flex flex-col md:flex-row">
         <div class=" max-w-full md:w-2/3 p-12 ">  <img class="h-autow-full rounded-lg" src="/images/spark1.png" alt="image description"></div>
@@ -121,7 +121,7 @@
 
   <div id="estandar-info"class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8  ">
     <div class="text-center">
-    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Gama Es</span>tándar</h1>
+    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Gama Estándar</span></h1>
     <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Motos de una altísima calidad que proporciona una buenísima estabilidad por su mayor tamaño. Eso no va a significar menos diversion ya que doblan en cv a la gama anterior.<a href="welcome"class="underline decoration-black-500"><b>Otros planes</b></a> </p> 
     <div class="flex flex-col md:flex-row">
       <div class=" max-w-full md:w-2/3 p-12 ">  <img class="h-autow-full rounded-lg" src="/images/gti1.png" alt="image description"></div>
@@ -169,7 +169,7 @@
  
 <div id="premium-info"class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8  ">
   <div class="text-center">
-  <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Gama Pre</span>mium</h1>
+  <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Gama Premium</span></h1>
   <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Lo más radical que se puede alquilar en toda España. Motos de competición que te hacen disfrutar al máximo de la velocida en el mar. Solo para expertos. <a href="welcome"class="underline decoration-black-500"><b>Otros planes</b></a> </p> 
   <div class="flex flex-col md:flex-row">
     <div class=" max-w-full md:w-2/3 p-12 ">  <img class="h-autow-full rounded-lg" src="/images/rtx.png" alt="image description"></div>
